314968e9d2c944e2a614d0c217d1b38fb753e7b0
ruiqurm <ruiqurm@gmail.com>
2022-06-08 12:05:44 +0800

    添加driver类

--- cs_server/driver.py
+++ cs_server/driver.py
@@ -1,6 +1,40 @@
 """
 全局的调度器
 """
-
+from .charge_station import ChargeStation
+from .transaction import Transaction
+from .scheduler import Scheduler
+import asyncio
+import threading
 class Driver:
-	pass
+	def __init__(self,sche:Scheduler) -> None:
+		self.scheduler = sche
+
+	def update_mode(self, tran:Transaction,mode)->None:
+		self.scheduler.on_update_mode(tran,mode)
+
+	def update_quantity(self, tran:Transaction,value)->None:
+		self.scheduler.on_update_mode(tran,value)
+
+	def cancel(self,tran:Transaction)->None:
+		self.scheduler.on_cancel(tran)
+
+	def signal_station_off(self,station:ChargeStation)->None:
+		# asyncio.create_task(self.scheduler.on_station_off())
+		t = threading.Thread(target=self.scheduler.on_station_off, args=(station,))
+		t.start()
+
+	def signal_station_on(self,station:ChargeStation)->None:
+		# asyncio.create_task(self.scheduler.on_station_on())
+		t = threading.Thread(target=self.scheduler.on_station_on, args=(station,))
+		t.start()
+
+	def signal_station_finish(self,station:ChargeStation)->None:
+		# asyncio.create_task(self.scheduler.on_station_on())
+		t = threading.Thread(target=self.scheduler.on_finish, args=(station,))
+		t.start()
+
+	def signal_station_error(self,station:ChargeStation)->None:
+		# asyncio.create_task(self.scheduler.on_station_on())
+		t = threading.Thread(target=self.scheduler.on_error, args=(station,))
+		t.start()		
