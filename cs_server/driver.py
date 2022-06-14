708df23b5c0f7f9dd27c3e9e5bb510eaab428c6a
ruiqurm <ruiqurm@gmail.com>
2022-06-14 16:05:36 +0800

    消除循环引用

--- cs_server/driver.py
+++ cs_server/driver.py
@@ -1,10 +1,15 @@
 """
 全局的调度器
 """
-from .charge_station import ChargeStation
-from .transaction import Transaction
-from .schedule import Scheduler
-import asyncio
+# from .charge_station import ChargeStation
+# from .transaction import Transaction
+# from .schedule import Scheduler
+from typing import TypeVar
+
+Scheduler = TypeVar('Scheduler')
+Transaction = TypeVar('Transaction')
+ChargeStation = TypeVar('ChargeStation')
+
 import threading
 class Driver:
 	def __init__(self,sche:Scheduler) -> None:
@@ -19,22 +24,22 @@ class Driver:
 	def cancel(self,tran:Transaction)->None:
 		self.scheduler.on_cancel(tran)
 
-	def signal_station_off(self,station:ChargeStation)->None:
+	def signal_station_off(self,station:'ChargeStation')->None:
 		# asyncio.create_task(self.scheduler.on_station_off())
 		t = threading.Thread(target=self.scheduler.on_station_off, args=(station.id,))
 		t.start()
 
-	def signal_station_on(self,station:ChargeStation)->None:
+	def signal_station_on(self,station:'ChargeStation')->None:
 		# asyncio.create_task(self.scheduler.on_station_on())
 		t = threading.Thread(target=self.scheduler.on_station_on, args=(station.id,))
 		t.start()
 
-	def signal_station_finish(self,station:ChargeStation)->None:
+	def signal_station_finish(self,station:'ChargeStation')->None:
 		# asyncio.create_task(self.scheduler.on_station_on())
 		t = threading.Thread(target=self.scheduler.on_finish, args=(station.id,))
 		t.start()
 
-	def signal_station_error(self,station:ChargeStation)->None:
+	def signal_station_error(self,station:'ChargeStation')->None:
 		# asyncio.create_task(self.scheduler.on_station_on())
 		t = threading.Thread(target=self.scheduler.on_error, args=(station.id,))
 		t.start()		
