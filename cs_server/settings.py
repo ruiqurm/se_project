4f7a6e84133bad2cfe323b8554230597278375ef
ruiqurm <ruiqurm@gmail.com>
2022-06-15 14:47:42 +0800

    修改配置

--- cs_server/settings.py
+++ cs_server/settings.py
@@ -13,15 +13,15 @@ class Settings:
     SECRET_KEY = b"wWMA-DqJEVi5s9bztbxHHanYUKKrocb-2mei0jrLQ8I="
     USER_LOGIN_TTL = 60 * 60 * 24
     # 快充基站数量
-    NUMBER_FC_STATION = 3
-    # 块冲基站充电速度
-    FC_STATION_SPEED = 5
+    NUMBER_FC_STATION = 2
+    # 快冲基站充电速度
+    FC_STATION_SPEED = 30
     # 慢充基站数量
     NUMBER_SC_STATION = 3
     # 慢冲基站充电速度
-    SC_STATION_SPEED = 2
+    SC_STATION_SPEED = 10
     # 等候区大小
-    WAITING_AREA_SIZE = 3
+    WAITING_AREA_SIZE = 10
 
     # **每个**充电桩后面队列长度
     CHARGE_AREA_QUEUE_SIZE = 3
@@ -48,7 +48,7 @@ class Settings:
     PERSIST_ID = False
 
     MOCK_DATETIME = True
-    START_TIME = datetime.time(hour=6, minute=30, second=0)
+    START_TIME = datetime.time(hour=6, minute=0, second=0)
     TIME_FLOW_RATE = 5
 
 
