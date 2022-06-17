b8f9e7df8db012a32b257362c7c4fa286ba53389
Crystal418-1 <81939870+Crystal418-1@users.noreply.github.com>
2022-06-18 00:54:31 +0800

    Update __main__.py
    加输出格式、说明，修改注释

--- cs_server/__main__.py
+++ cs_server/__main__.py
@@ -71,7 +71,7 @@ async def startup_event():
 
 @app.post("/register",response_model=serializers.User)
 def register(username: str, password: str) -> User:
-    """注册
+    """注册 \n
     username: 用户名 \n
     password: 密码 \n
     返回注册成功的用户
@@ -80,9 +80,9 @@ def register(username: str, password: str) -> User:
     return u
 
 
-@app.post("/login")
+@app.post("/login", response_model=str)
 def login(username: str, password: str) -> str:
-    """登录
+    """登录 \n
     username: 用户名 \n
     password: 密码 \n
     返回一个token
@@ -92,14 +92,21 @@ def login(username: str, password: str) -> str:
 
 @app.post("/transaction")
 def transaction(Id: str, charge_mode: int, charge_quantity: float) -> None:
-    "提交充电请求"
+    """提交充电请求 \n
+    Id: 用户id \n
+    charge_mode: 充电模式 \n
+    charge_quantity: 充电电量 \n
+    """
     u = User.get_user(Id)
     driver.push(u, charge_mode, charge_quantity)
 
 
 @app.post("/updateMode")
 def updateMode(Id: str, new_mode: str) -> None:
-    "修改充电模式"
+    """修改充电模式 \n
+    Id: 用户id \n
+    new_mode: 新充电模式 \n
+    """
     u = User.get_user(Id)
     t = u.get_running_transaction()
     driver.update_mode(t, new_mode)
@@ -107,7 +114,10 @@ def updateMode(Id: str, new_mode: str) -> None:
 
 @app.post("/updateQuantity")
 def updateQuantity(Id: str, new_quantity: float) -> None:
-    "修改充电量"
+    """修改充电量 \n
+    Id: 用户id \n
+    new_quantity: 新充电量 \n
+    """
     u = User.get_user(Id)
     t = u.get_running_transaction()
     driver.update_quantity(t, new_quantity)
@@ -115,64 +125,86 @@ def updateQuantity(Id: str, new_quantity: float) -> None:
 
 @app.post("/cancel")
 def cancel(Id: str) -> None:
-    "取消充电请求"
+    """取消充电请求 \n
+    Id: 用户id \n
+    """
     u = User.get_user(Id)
     t = u.get_running_transaction()
     driver.signal_station_cancel(t)
 
 
-@app.post("/getTransaction")
+@app.post("/getTransaction", response_model=str)
 def getTransaction(Id: str) -> str:
-    "查看本车排队号码"
+    """查看本车排队号码 \n
+    Id: 用户id \n
+    返回排队号码
+    """
     u = User.get_user(Id)
     t = u.get_running_transaction()
     return t.wait_id
 
 
-@app.post("/getWaiting")
+@app.post("/getWaiting", response_model=int)
 def getWaiting(Id: str) -> int:
-    "查看本充电模式下前车等待数量"
+    """查看本充电模式下前车等待数量 \n
+    Id: 用户id \n
+    返回前车等待数量
+    """
     u = User.get_user(Id)
     t = u.get_running_transaction()
     return scheduler.areaMngt.get_waiting(t.id)
 
 
-@app.post("/getBills")
+@app.post("/getBills", response_model=List[serializers.Bill])
 def getBills(Id: str) -> List[Bill]:
-    "查看充电详单"
+    """查看充电详单 \n
+    Id: 用户id \n
+    返回帐单列表
+    """
     u = User.get_user(Id)
     return u.get_all_bills()
 
 
 @app.post("/stationOn")
 def stationOn(stationId: int) -> None:
-    "启动充电桩"
+    """启动充电桩 \n
+    stationId: 充电桩id \n
+    """
     station_mgmt.turn_on(stationId)
 
 
 @app.post("/stationOff")
 def stationOff(stationId: int) -> None:
-    "关闭充电桩"
+    """关闭充电桩 \n
+    stationId: 充电桩id \n
+    """
     station_mgmt.turn_off(stationId)
 
 
-@app.post("/stationStatus")
+@app.post("/stationStatus", response_model=List[serializers.StationStatus])
 def stationStatus() -> List[StationStatus]:
-    "充电桩状态"
+    """查看充电桩状态 \n
+    返回充电桩状态列表
+    """
     return station_mgmt.get_status()
 
 
-@app.post("/stationId")
-def stationId(stationId: int):
-    "每个充电柱车辆"
+@app.post("/stationId", response_model=List[serializers.User])
+def stationId(stationId: int) -> List[User]:
+    """查看充电桩车辆 \n
+    stationId: 充电桩id \n
+    返回充电桩车辆
+    """
     return scheduler.areaMngt.get_charging(stationId)
 
 
 @app.get("/statistics")
 def statistics():
-    "数据统计"
+    """数据统计
+    """
     return "statistics"
 
 
 if __name__ == '__main__':
     uvicorn.run(app)
+
